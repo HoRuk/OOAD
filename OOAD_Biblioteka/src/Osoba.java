9dcb2a1574d2228c06fba1906081135dda17cc84
Hadzem <hhadzic2@etf.unsa.ba>
2015-05-04 01:18:42 +0200

    Početak

---
+++ OOAD_Biblioteka/src/Osoba.java
@@ -0,0 +1,20 @@
+public class Osoba {
+	protected String ime;
+	protected String prezime;
+	
+	public void setIme(String ime){
+		this.ime = ime;
+	}
+	
+	public String getIme(){
+		return ime;
+	}
+	
+	public void setPrezime(String prezime){
+		this.prezime = prezime;
+	}
+	
+	public String getPrezime(){
+		return prezime;
+	}
+}
---
+++ OOAD_Biblioteka/src/Uposlenik.java
@@ -0,0 +1,11 @@
+public class Uposlenik extends Osoba{
+	protected Kartica kartica;
+	
+	public void setKartica(Kartica kartica){
+		this.kartica = kartica;
+	}
+	
+	public Kartica getKartica(){
+		return this.kartica;
+	}
+}
